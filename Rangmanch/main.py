8edc460f2abd3ae5a2db3289fc2789f21b80fbd5
4nkit-3isGGS <codewithankit487@gmail.com>
2026-09-24 00:38:11 +0530

    Configure app lifespan with database initialization and register review router

--- Rangmanch/main.py
+++ Rangmanch/main.py
@@ -1,5 +1,25 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from database import create_tables
+from routes import review as review_routes
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_tables()
+    print("Database Starting up...")
+    yield
+    print("Shutting Down Database...")
 
 app = FastAPI(
-    title= "RangManch"
+    title= "Rangmanch",
+    description= "A Theatre review API for Pune Rangmanch.",
+    lifespan=lifespan
 )
+
+app.include_router(review_routes.router)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to Rangmanch Review API."
+    }
